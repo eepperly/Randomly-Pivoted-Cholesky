8b5010131f8807cf389cd63d7bdf8d7d5554f2f3
Ethan Epperly <epperlyethan@gmail.com>
2022-07-11 15:06:03 -0700

    Add DPP/QML/QM9 to setup script

--- setup.sh
+++ setup.sh
@@ -2,3 +2,12 @@
 
 wget https://raw.githubusercontent.com/axelv/recursive-nystrom/master/recursive_nystrom.py
 
+pip install qml
+pip install dppy
+
+wget https://figshare.com/ndownloader/files/3195389
+mkdir -p molecules
+cd molecules
+mv ../3195389 .
+tar -xvf 3195389
+

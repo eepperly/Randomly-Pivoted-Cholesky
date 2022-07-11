894db55ce9861874c263b5db0504f55393b4e7a9
Ethan Epperly <epperlyethan@gmail.com>
2022-07-11 14:10:09 -0700

    Add code for greedy and RPCholesky

---
+++ lra.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+from abc import ABC, abstractmethod
+
+class AbstractPSDLowRank(ABC):
+
+    def __init__(self, **kwargs):
+        self.idx = kwargs['idx'] if ('idx' in kwargs) else None
+        self.rows = kwargs['rows'] if ('rows' in kwargs) else None
+
+    @abstractmethod
+    def trace(self):
+        pass
+
+    @abstractmethod
+    def __matmul__(self, other):
+        pass
+
+    def __rmatmul__(self, other):
+        return (self @ other.T).T
+
+    @abstractmethod
+    def rank(self):
+        pass
+
+    @abstractmethod
+    def eigenvalue_decomposition(self):
+        pass
+
+    def matrix(self):
+        return self @ np.identity(self.shape[0])
+
+class CompactEigenvalueDecomposition(AbstractPSDLowRank):
+
+    def __init__(self, V, Lambda, **kwargs):
+        super().__init__(**kwargs)
+        self.V = V
+        self.Lambda = Lambda
+        self.shape = (self.V[0], self.V[0])
+
+    @staticmethod
+    def from_F(F, **kwargs):
+        Q, R = np.linalg.qr(F, "reduced")
+        U, S, _ = np.linalg.svd(R)
+        return CompactEigenvalueDecomposition(Q @ U, S ** 2, **kwargs)
+
+    def trace(self):
+        return sum(self.Lambda)
+
+    def __matmul__(self, other):
+        return self.V @ (self.Lambda * (self.V.T @ other))
+
+    def rank(self):
+        return len(S)
+
+    def matrix(self):
+        return self.V @ (self.Lambda * self.V.T)
+
+    def eigenvalue_decomposition(self):
+        return self
+
+    def krr(self, b, lam):
+        VTb = self.V.T @ b
+        return b / lam + self.V @ (np.divide(VTb, self.Lambda.reshape(VTb.shape) + lam) - VTb / lam)
+    
+    def krr_vec(self,b,lam):
+        VTb = self.V.T @ b
+        diag = self.Lambda.reshape(VTb.shape[0]) + lam
+        return b / lam + self.V @ (np.divide(VTb, diag[:,np.newaxis]) - VTb / lam)
+        
+class PSDLowRank(AbstractPSDLowRank):
+
+    def __init__(self, F, **kwargs):
+        super().__init__(**kwargs)
+        self.F = F
+        self.shape = (F.shape[0],F.shape[0])
+
+    def trace(self):
+        return np.linalg.norm(self.F, 'fro')**2
+
+    def __matmul__(self,other):
+        return self.F@(self.F.T@other)
+
+    def rank(self):
+        return self.F.shape[1]
+
+    def matrix(self):
+        return self.F @ self.F.T
+
+    def eigenvalue_decomposition(self):
+        return CompactEigenvalueDecomposition.from_F(self.F, idx = self.idx, rows = self.rows)
+
+class NystromExtension(AbstractPSDLowRank):
+
+    def __init__(self, F, C, factor = None, **kwargs):
+        super().__init__(**kwargs)
+        self.F = F
+        self.C = (C+C.T)/2
+        self.shape = (self.F[0], self.F[0])
+        self.factor = factor
+
+    def get_factor(self):
+        if self.factor is None:
+            L = np.linalg.cholesky(self.C+100*self.C.max()*np.finfo(float).eps*np.identity(self.C.shape[0]))
+            self.factor = np.linalg.solve(L, self.F.T).T 
+        return self.factor
+        
+    def trace(self):
+        return np.linalg.norm(self.get_factor())**2
+
+    def __matmul__(self, other):
+        return self.F@np.linalg.solve(self.C,self.F.T@other)
+
+    def rank(self):
+        return self.C.shape[0]
+
+    def matrix(self):
+        return self.F@np.linalg.solve(self.C,self.F.T)
+    
+    def eigenvalue_decomposition(self):
+        return CompactEigenvalueDecomposition.from_F(self.get_factor(), self.idx = idx, self.rows = rows)
---
+++ rp_cholesky.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from lra import PSDLowRank
+
+def cholesky_helper(A, n, k, alg):
+    diags = A.diag()
+    
+    # row ordering, is much faster for large scale problems
+    F = np.zeros((k,n))
+    rows = np.zeros((k,n))
+    rng = np.random.default_rng()
+    
+    arr_idx = []
+    
+    for i in range(k):
+        if alg == 'rp':
+            idx = rng.choice(range(n), p = diags / sum(diags))
+        elif alg == 'greedy':
+            idx = np.argmax(diags)
+        else:
+            raise RuntimeError("Algorithm '{}' not recognized".format(alg))
+
+        arr_idx.append(idx)
+        rows[i,:] = A[idx,:]
+        F[i,:] = (rows[i,:] - F[:i,idx].T @ F[:i,:]) / np.sqrt(diags[idx])
+        diags -= F[i,:]**2
+        diags = diags.clip(min = 0)
+
+    return PSDLowRank(F.T, idx = arr_idx, cols = rows)
+
+def block_cholesky_helper(A, n, k, b, alg):
+    diags = A.diag()
+    
+    # row ordering
+    F = np.zeros((k,n))
+    rows = np.zeros((k,n))
+    
+    rng = np.random.default_rng()
+
+    arr_idx = []
+    
+    cols = 0
+    while cols < k:
+        block_size = min(k-cols, b)
+        
+        if alg == 'rp':
+            idx = rng.choice(range(n), size = 2*block_size, p = diags / sum(diags),replace=False)
+            idx = np.unique(idx)[:block_size]
+            block_size = len(idx)
+        elif alg == 'greedy':
+            idx = np.argpartition(diags, -block_size)[-block_size:]
+        else:
+            raise RuntimeError("Algorithm '{}' not recognized".format(alg))
+
+        arr_idx.extend(idx)
+        rows[cols:cols+block_size,:] = A[idx,:]
+        F[cols:cols+block_size,:] = rows[cols:cols+block_size,:] - F[0:cols,idx].T @ F[0:cols,:]
+        C = F[cols:cols+block_size,idx]
+        L = np.linalg.cholesky(C+100*np.finfo(float).eps*np.identity(block_size))
+        F[cols:cols+block_size,:] = np.linalg.solve(L, F[cols:cols+block_size,:])
+        diags -= np.sum(F[cols:cols+block_size,:]**2, axis=0)
+        diags = diags.clip(min = 0)
+
+        cols += block_size
+
+    return PSDLowRank(F.T, idx = arr_idx, rows = rows)
+
+def rp_cholesky(A, n, k):
+    return cholesky_helper(A, n, k, 'rp')
+
+def greedy(A, n, k):
+    return cholesky_helper(A, n, k, 'greedy')
+
+def block_rp_cholesky(A, n, k, b = 100):
+    return block_cholesky_helper(A, n, k, b, 'rp')
+
+def block_greedy(A, n, k, b = 100):
+    return block_cholesky_helper(A, n, k, b, 'greedy')

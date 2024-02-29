3595049d5fac188c5e90a3cd25b9a82b6f53ab66
Ethan Epperly <eepperly@caltech.edu>
2024-02-29 13:50:35 -0800

    Add robust block random pivoting

--- rpcholesky.py
+++ rpcholesky.py
@@ -5,6 +5,8 @@ import scipy as sp
 from lra import PSDLowRank
 from warnings import warn
 
+greedy_lapack = sp.linalg.get_lapack_funcs('pstrf', dtype=np.float64)
+
 def cholesky_helper(A, k, alg, stoptol = 0):
     n = A.shape[0]
     diags = A.diag()
@@ -42,7 +44,16 @@ def cholesky_helper(A, k, alg, stoptol = 0):
 
     return PSDLowRank(G, idx = arr_idx, rows = rows)
 
-def block_cholesky_helper(A, k, b, alg, stoptol = 1e-14):
+def _greedy_cholesky(A, tol = None):
+    L, piv, rank, info = greedy_lapack(A, lower=True)
+    L = np.tril(L)
+    if not (tol is None):
+        index = np.where(np.abs(np.diagonal(L)) < np.sqrt(tol))[0]
+        if len(index) > 0:
+            rank = index[0]
+    return L, piv, rank
+
+def block_cholesky_helper(A, k, b, alg, stoptol = 1e-14, strategy = "regularize"):
     diags = A.diag()
     n = A.shape[0]
     orig_trace = sum(diags)
@@ -71,23 +82,40 @@ def block_cholesky_helper(A, k, b, alg, stoptol = 1e-14):
         else:
             raise RuntimeError("Algorithm '{}' not recognized".format(alg))
 
-        arr_idx.extend(idx)
-        rows[counter:counter+block_size,:] = A[idx,:]
-        G[counter:counter+block_size,:] = rows[counter:counter+block_size,:] - G[0:counter,idx].T @ G[0:counter,:]
-        C = G[counter:counter+block_size,idx]
-        try:
-            L = np.linalg.cholesky(C + np.finfo(float).eps*b*scale*np.identity(block_size))
-            G[counter:counter+block_size,:] = np.linalg.solve(L, G[counter:counter+block_size,:])
-        except np.linalg.LinAlgError:
-            warn("Cholesky failed in block partial Cholesky. Falling back to eigendecomposition")
-            evals, evecs = np.linalg.eigh(C)
-            evals[evals > 0] = evals[evals > 0] ** (-0.5)
-            evals[evals < 0] = 0
-            G[counter:counter+block_size,:] = evals[:,np.newaxis] * (evecs.T @ G[counter:counter+block_size,:])
-        diags -= np.sum(G[counter:counter+block_size,:]**2, axis=0)
+        if "regularize" == strategy or "regularized" == strategy:
+            arr_idx.extend(idx)
+            rows[counter:counter+block_size,:] = A[idx,:]
+            G[counter:counter+block_size,:] = rows[counter:counter+block_size,:] - G[0:counter,idx].T @ G[0:counter,:]
+            C = G[counter:counter+block_size,idx]
+        
+            try:
+                L = np.linalg.cholesky(C + np.finfo(float).eps*b*scale*np.identity(block_size))
+                G[counter:counter+block_size,:] = np.linalg.solve(L, G[counter:counter+block_size,:])
+                
+            except np.linalg.LinAlgError:
+                warn("Cholesky failed in block partial Cholesky. Falling back to eigendecomposition")
+                evals, evecs = np.linalg.eigh(C)
+                evals[evals > 0] = evals[evals > 0] ** (-0.5)
+                evals[evals < 0] = 0
+                G[counter:counter+block_size,:] = evals[:,np.newaxis] * (evecs.T @ G[counter:counter+block_size,:])
+
+        elif "pivoting" == strategy or "pivoted" == strategy:
+            H = A[idx,idx] - G[0:counter,idx].T @ G[0:counter,idx]
+            # L, piv, rank = _greedy_cholesky(H, tol = np.finfo(float).eps*b*scale)
+            L, piv, rank = _greedy_cholesky(H, tol = 1e-14)
+            idx = idx[piv[0:rank]-1]
+            arr_idx.extend(idx)
+            rows[counter:counter+len(idx),:] = A[idx,:]
+            G[counter:counter+len(idx),:] = rows[counter:counter+len(idx),:] - G[0:counter,idx].T @ G[0:counter,:]            
+            G[counter:counter+len(idx),:] = np.linalg.solve(L[0:rank,0:rank], G[counter:counter+len(idx),:])
+
+        else:
+            raise ValueError("'{}' is not a valid strategy for block RPCholesky".format(strategy))
+                
+        diags -= np.sum(G[counter:counter+len(idx),:]**2, axis=0)
         diags = diags.clip(min = 0)
 
-        counter += block_size
+        counter += len(idx)
 
         if stoptol > 0 and sum(diags) <= stoptol * orig_trace:
             G = G[:counter,:]
@@ -100,6 +128,8 @@ def rejection_cholesky(H):
     b = H.shape[0]
     if H.shape[0] != H.shape[1]:
         raise RuntimeError("rejection_cholesky requires a square matrix")
+    if np.trace(H) <= 0:
+        raise RuntimeError("rejection_cholesky requires a strictly positive trace")
     u = np.array([H[j,j] for j in range(b)])
 
     idx = []
@@ -113,12 +143,12 @@ def rejection_cholesky(H):
     L = L[np.ix_(idx,idx)]
     return L, idx
 
-def accelerated_rpcholesky(A, k, b = 100, stoptol = 1e-14):
+def accelerated_rpcholesky(A, k, b = 100, stoptol = 1e-13):
     diags = A.diag()
     n = A.shape[0]
     orig_trace = sum(diags)
     if stoptol is None:
-        stoptol = 1e-14
+        stoptol = 1e-13
     
     # row ordering
     G = np.zeros((k,n))
@@ -158,27 +188,27 @@ def accelerated_rpcholesky(A, k, b = 100, stoptol = 1e-14):
 
     return PSDLowRank(G, idx = arr_idx, rows = rows)
 
-def rpcholesky(A, k, b = None, accelerated = False, stoptol = None):
+def rpcholesky(A, k, b = None, accelerated = False, **kwargs):
     if b is None:
         if accelerated:
-            return accelerated_rpcholesky(A, k, stoptol = stoptol)
+            return accelerated_rpcholesky(A, k, **kwargs)
         else:
-            return cholesky_helper(A, k, 'rp', stoptol = stoptol)
+            return cholesky_helper(A, k, 'rp', **kwargs)
     elif accelerated:
-        return accelerated_rpcholesky(A, k, b, stoptol = stoptol)
+        return accelerated_rpcholesky(A, k, b, **kwargs)
     else:
-        return block_cholesky_helper(A, k, b, 'rp', stoptol = stoptol)
+        return block_cholesky_helper(A, k, b, 'rp', **kwargs)
 
-def greedy(A, k, randomized_tiebreaking = False, b = 1, stoptol = None):
+def greedy(A, k, randomized_tiebreaking = False, b = 1, **kwargs):
     if b == 1:
-        return cholesky_helper(A, k, 'rgreedy' if randomized_tiebreaking else 'greedy', stoptol = stoptol)
+        return cholesky_helper(A, k, 'rgreedy' if randomized_tiebreaking else 'greedy', **kwargs)
     else:
         if randomized_tiebreaking:
             warn("Randomized tiebreaking not implemented for block greedy method")
-        return block_cholesky_helper(A, k, b, 'greedy', stoptol = stoptol)
+        return block_cholesky_helper(A, k, b, 'greedy', **kwargs)
 
-def block_rpcholesky(A, k, b = 100, stoptol = None):
-    return block_cholesky_helper(A, k, b, 'rp', stoptol = stoptol)
+def block_rpcholesky(A, k, b = 100, **kwargs):
+    return block_cholesky_helper(A, k, b, 'rp', **kwargs)
 
 def block_greedy(A, k, b = 100, stoptol = None):
-    return block_cholesky_helper(A, k, b, 'greedy', stoptol = stoptol)
+    return block_cholesky_helper(A, k, b, 'greedy', **kwargs)

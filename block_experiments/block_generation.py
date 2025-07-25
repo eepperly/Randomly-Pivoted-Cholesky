6cd129d235fcad7cd3e862c3bb884c065b2cf67b
Ethan Epperly <eepperly@caltech.edu>
2025-07-24 22:27:52 -0700

    Clean up block_generation script

--- block_experiments/block_generation.py
+++ block_experiments/block_generation.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
+import sys
+sys.path.append('../')
+
 from matrix import KernelMatrix
 import numpy as np
 from time import time
+from scipy.io import savemat
 
 N = int(1e5)
 bs = np.ceil(np.logspace(0,3,30))
 bs = np.unique(np.array(bs, dtype=int))
 trials = 100
+ds = [1,10,100,1000]
+
+all_data = {}
 
 for kernelname in ["gaussian", "laplace"]:
-    for d in [1,10,100,1000]:
+    data = np.zeros((len(ds)*len(bs),3))
+    i = 0
+    for d in ds:
         X = np.random.randn(N, d)
         A = KernelMatrix(X, kernelname)
         for b in bs:
@@ -22,4 +31,15 @@ for kernelname in ["gaussian", "laplace"]:
                 stop = time()
                 avg += (stop - start) / trials
             print("{}\t{}\t{}\t{}".format(kernelname,d,b,avg/b))
+            data[i,0] = d
+            data[i,1] = b
+            data[i,2] = avg/b
+            i += 1
+
+    all_data[kernelname] = data
+
+import os
+os.makedirs("data", exist_ok=True)
+    
+savemat("data/generation.mat", all_data)
 
